c037f880f4bc7effd8ab4d57811a78e8d20c514a
Andres De Innocentiis <79876962+andresdeinnocentiis@users.noreply.github.com>
2021-09-26 22:50:51 -0300

    Update ejercicio2.py

--- ejercicios_practica/ejercicio2.py
+++ ejercicios_practica/ejercicio2.py
@@ -13,12 +13,37 @@ import requests
 import matplotlib.pyplot as plt
 
 
+
+
+
+def title_x_user(url:str)->dict:
+    dic_alumnos = {}
+    response = requests.get(url)
+    data = response.json()
+     
+    for dic in data:
+        
+        user = str(dic.get("userId"))
+        if f"User{user}" not in dic_alumnos.keys():
+            dic_alumnos[f"User{user}"] = {}
+            dic_alumnos[f"User{user}"]["cant_titulos"] = 0
+            dic_alumnos[f"User{user}"]["titulos"] = []
+            
+        if dic.get("completed") == True:
+            dic_alumnos[f"User{user}"]["cant_titulos"] += 1
+            dic_alumnos[f"User{user}"]["titulos"].append(dic.get("title"))  
+        
+    return dic_alumnos
+
+
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
     
     # Ejercicio de consumo de datos por API
     url = "https://jsonplaceholder.typicode.com/todos"
-
+    
+    
+    
     # El primer paso es que copien esa URL en su explorador web
     # y analicen los datos en general:
     # 1) Observando la URL se puede ver que en total hay 200 entradas,
@@ -27,14 +52,18 @@ if __name__ == '__main__':
     # del userId=1 al userId=10
     # 3) En cada entrada se especifica si el usuario completó ese título,
     # mediante el campo "completed".
-
-
+    
+    dic_alumnos = title_x_user(url)
+    print(dic_alumnos)
+    json_alumnos = json.dumps(dic_alumnos, indent=4)
+    print(json_alumnos)
+    
     # Alumno, de cada usuario en el total de las 200 entradas
     # debe contar cuantos títulos completó cada usuario (de los 10 posibles)
     # y armar un gráfico de barras resumiendo la información.
     # gráfico en el eje "x" está cada uno de los 10 usuarios y en el eje
     # "y" la cantidad de títulos completados
-
+    
     # Para poder ir haciendo esto debe ir almacenando la información
     # de cada usuario a medida que "itera" en un bucle los datos
     # del JSON recolectado. Al finalizar el bucle deberá tener la data
@@ -46,4 +75,16 @@ if __name__ == '__main__':
     # y verifique si los primeros usuarios (mirando la página a ojo)
     # los datos recolectados son correctos.
 
+    usuarios = [x for x in dic_alumnos.keys()]
+    cantidad_titulos = [valor.get("cant_titulos") for user,valor in dic_alumnos.items()]
+    fig = plt.figure()
+    fig.suptitle('Titulos por usuario', fontsize=14)
+    ax = fig.add_subplot()
+    ax.bar(usuarios, cantidad_titulos, label='usuarios',color='green')
+    ax.set_facecolor('whitesmoke')
+    ax.legend()
+    plt.show()
+    
+
+    
     print("terminamos")
